c6a3123c0f10a1f5e4b0c824b66ca767a61d529a
peytontw <wilkiejr29@gmail.com>
2017-12-09 23:43:24 +0000

    Changing gears

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Comment = mongoose.model('Comment');
+var expressSession = require('express-session');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var users = require('../controllers/users_controller');
+console.log("before / Route");
+router.get('/', function(req, res){
+    console.log("/ Route");
+//    console.log(req);
+    console.log(req.session);
+    if (req.session.user) {
+      console.log("/ Route if user");
+      res.render('index', {username: req.session.username,
+                           msg:req.session.msg,
+                           color:req.session.color});
+    } else {
+      console.log("/ Route else user");
+      req.session.msg = 'Access denied!';
+      res.redirect('/login');
+    }
 });
-
-router.post('/comments', function(req, res, next) {
-  var comment = new Comment(req.body);
-  comment.save(function(err, comment){
-    if(err){ return next(err); }
-    res.json(comment);
+router.get('/user', function(req, res){
+    console.log("/user Route");
+    if (req.session.user) {
+      res.render('user', {msg:req.session.msg});
+    } else {
+      req.session.msg = 'Access denied!';
+      res.redirect('/login');
+    }
+});
+router.get('/signup', function(req, res){
+    console.log("/signup Route");
+    if(req.session.user){
+      res.redirect('/');
+    }
+    res.render('signup', {msg:req.session.msg});
+});
+router.get('/login',  function(req, res){
+    console.log("/login Route");
+    if(req.session.user){
+      res.redirect('/');
+    }
+    res.render('login', {msg:req.session.msg});
+});
+router.get('/logout', function(req, res){
+    console.log("/logout Route");
+    req.session.destroy(function(){
+      res.redirect('/login');
+    });
   });
-});
+router.post('/signup', users.signup);
+router.post('/user/update', users.updateUser);
+router.post('/user/delete', users.deleteUser);
+router.post('/login', users.login);
+router.get('/user/profile', users.getUserProfile);
 
-router.get('/comments', function(req, res, next) {
-  Comment.find(function(err, comments) {
-    if(err){ return next(err); }
-    res.json(comments);
-  });
-});
-
-router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id);
-  query.exec(function (err, comment){
-    if (err) { return next(err); }
-    if (!comment) { return next(new Error("can't find comment")); }
-    req.comment = comment;
-    return next();
-  });
-});
-
-router.get('/comments/:comment', function(req, res) {
-  res.json(req.comment);
-});
-
-router.put('/comments/:comment/upvote', function(req, res, next) {
-  req.comment.upvote(function(err, comment){
-    if (err) { return next(err); }
-    res.json(comment);
-  });
-});
-
-router.delete('/comments/:comment', function(req, res) {
-  console.log("in Delete");
-  req.comment.remove();
-  res.sendStatus(200);
-});
 
 module.exports = router;
